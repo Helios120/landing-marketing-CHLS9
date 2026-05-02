b675d409dc586f81961c341c4004b5a8a50e82c2
Helios120 <79373191+Helios120@users.noreply.github.com>
2026-05-02 13:10:15 +0200

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+  mint: "EffrWV33wCbHNXbTJ7T7H5AsfxuXjB7wACHgM6JAGYd6",
+  presalePrice: 0.1,
+  presaleTarget: 20,
+  presaleAllocations: 100,
+  vesting: {
+    cliff: 14 * 24 * 3600,
+    duration: 90 * 24 * 3600
+  }
+};
